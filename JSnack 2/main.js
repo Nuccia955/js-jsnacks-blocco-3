090067283af3194a098d08c308e524a81bb00126
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-08 15:36:02 +0100

    add jsnack 2

--- JSnack 2/main.js
+++ JSnack 2/main.js
@@ -1,2 +1,8 @@
 /* A partire da un array di numeri, genera un secondo array con le potenze al quadrato di ogni numero.
 Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25] */
+
+function genSquares (element) {
+    return element.map(el => el * el);
+}
+
+console.log(genSquares([1,10,5,9]));
